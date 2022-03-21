36096d903b353e08ca067b1dbc9eedb722fd0370
Nirali Tandel <nirali.tandel@1Rintern.local>
2022-03-21 12:55:52 +0530

    Updated

--- AngularDemo/src/app/modules/user-subject/user-subject-routing.module.ts
+++ AngularDemo/src/app/modules/user-subject/user-subject-routing.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserListComponent } from './user-list/user-list.component';
 import { UserSubjectComponent } from './user-subject.component';
 
 const routes: Routes = [
-  { path: '', component: UserSubjectComponent,
-  children: [
-    { path: '', component: UserListComponent },
-    { path: 'add', component: UserFormComponent },
-    { path: 'cancel', component: UserListComponent },
-    { path: 'edit/:id', component: UserFormComponent }
-  ]
-}
+  { path: '', component: UserSubjectComponent }
 ];
 
 @NgModule({
